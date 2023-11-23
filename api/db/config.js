38854b9d0fcf5db8ca8a29e5b3d5930e7e7431c6
jourdanmauricio <jourdanmauricio@gmail.com>
2023-11-23 08:09:05 -0300

    .

--- api/db/config.js
+++ api/db/config.js
@@ -1,17 +1,11 @@
 const { config } = require('../config/config');
 
-// const USER = encodeURIComponent(config.dbUser);
-// const PASS = encodeURIComponent(config.dbPass);
-// const URI = `postgres://${USER}:${PASS}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
 module.exports = {
   development: {
-    //url: URI,
     url: config.dbUrl,
     dialect: 'postgres',
   },
   production: {
-    // url: URI,
     url: config.dbUrl,
     dialect: 'postgres',
     dialectOptions: {
--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -2,10 +2,6 @@ const { Sequelize } = require('sequelize');
 const { config } = require('../config/config');
 const setupModels = require('../db/models');
 
-// const USER = encodeURIComponent(config.dbUser);
-// const PASS = encodeURIComponent(config.dbPass);
-// const URI = `postgres://${USER}:${PASS}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
 const URI = config.dbUrl;
 
 const options = {
